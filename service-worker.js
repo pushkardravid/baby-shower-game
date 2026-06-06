9ab54a9f69a67e7f8b7f26db93c92b5a947e8dfa
Pushkar Dravid <36061658+pushkardravid@users.noreply.github.com>
2026-06-06 01:23:08 -0700

    v4 changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
-const CACHE_NAME = "design-the-baby-v2";
-const FILES = ["./", "./index.html", "./manifest.json", "./service-worker.js"];
+const CACHE_NAME = "design-the-baby-v4";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./assets/mom.jpg",
+  "./assets/dad.jpg",
+  "./assets/mom_eyes.jpg",
+  "./assets/dad_eyes.jpg",
+  "./assets/mom_hair.jpg",
+  "./assets/dad_hair.jpg",
+  "./assets/mom_nose.jpg",
+  "./assets/dad_nose.jpg",
+  "./assets/mom_smile.jpg",
+  "./assets/dad_smile.jpg",
+  "./assets/mom_face.jpg",
+  "./assets/dad_face.jpg"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
