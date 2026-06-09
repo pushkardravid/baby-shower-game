13a42a93ebe1a2446f44c1a13245fafa9499bea1
Pushkar Dravid <36061658+pushkardravid@users.noreply.github.com>
2026-06-08 19:29:50 -0700

    v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "moms-eyes-dads-smile-v10";
+const CACHE_NAME = "moms-eyes-dads-smile-v12";
 const FILES = ["./","./index.html","./manifest.json","./service-worker.js","./assets/mom.jpg","./assets/dad.jpg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))));
 self.addEventListener("fetch", event => {
