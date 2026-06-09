2a346b87504d56e6cf3f7d504541e6c1420e64b9
Pushkar Dravid <36061658+pushkardravid@users.noreply.github.com>
2026-06-08 18:21:54 -0700

    v9 incoming

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,6 @@
-const CACHE_NAME = "moms-eyes-dads-smile-v8";
+const CACHE_NAME = "moms-eyes-dads-smile-v9";
 const FILES = ["./","./index.html","./manifest.json","./service-worker.js","./assets/mom.jpg","./assets/dad.jpg"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-});
-
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))));
 self.addEventListener("fetch", event => {
   if (event.request.url.includes("/api/")) return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
